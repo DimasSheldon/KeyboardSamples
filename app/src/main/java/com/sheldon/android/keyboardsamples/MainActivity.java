bc3cbf7770612d824b64ecdebb09b02add94b928
Dimas Sheldon <dim.sheldon@gmail.com>
2018-02-15 18:00:58 +0700

    Initial commit

---
+++ app/src/main/java/com/sheldon/android/keyboardsamples/MainActivity.java
@@ -0,0 +1,48 @@
+package com.sheldon.android.keyboardsamples;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void showText(View view) {
+        EditText editText = (EditText) findViewById(R.id.editText_main);
+        if (editText != null) {
+            String showString = editText.getText().toString();
+            Toast.makeText(this, showString, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void showUsername(View view) {
+        EditText editText = (EditText) findViewById(R.id.editText_username);
+        if (editText != null) {
+            String showString = editText.getText().toString();
+            Toast.makeText(this, showString, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void showEmail(View view) {
+        EditText editText = (EditText) findViewById(R.id.editText_email);
+        if (editText != null) {
+            String showString = editText.getText().toString();
+            Toast.makeText(this, showString, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void showPhoneNumber(View view) {
+        EditText editText = (EditText) findViewById(R.id.editText_phone);
+        if (editText != null) {
+            String showString = editText.getText().toString();
+            Toast.makeText(this, showString, Toast.LENGTH_SHORT).show();
+        }
+    }
+}
